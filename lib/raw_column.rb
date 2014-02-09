2a4a1e50fd9e2fd68efe58c30aec88f7c06dbac9
Tony Schneider <tonywok@gmail.com>
2014-02-09 12:48:51 -0500

    Extract json proxy code from project

--- lib/raw_column.rb
+++ lib/raw_column.rb
@@ -1,4 +1,21 @@
 require "raw_column/version"
+require "raw_column/json_proxy"
 
 module RawColumn
+
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def raw_column(*column_names)
+      class_eval do
+        column_names.each do |column_name|
+          define_method :"raw_#{column_name}" do
+            JsonProxy.new(self, column_name)
+          end
+        end
+      end
+    end
+
+  end
 end
---
+++ lib/raw_column/json_proxy.rb
@@ -0,0 +1,18 @@
+module RawColumn
+
+  class JsonProxy
+
+    def initialize(record, column_name=:content)
+      @raw_content = record.send(:"#{column_name}_before_type_cast")
+    end
+
+    def as_json(*args)
+      self
+    end
+
+    def encode_json(encoder)
+      @raw_content
+    end
+
+  end
+end
